065051997f0e3ed3f1ab25968453fdd4c80d7e5d
Letam Bossman Barinua <letambarinua@gmail.com>
2022-09-28 13:25:41 +0100

    getShipCoordinates test passed

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-plusplus */
 function getShipCoordinates(array, length, align) {
   const shipCoordinates = [array];
-  if (align === "Y") {
+  if (align === "X") {
     for (let i = 1; i < length; i++) {
-      shipCoordinates.push([array[0] + i, array[1]]);
+      shipCoordinates.push([array[0], array[1] + i]);
     }
   } else {
     for (let i = 1; i < length; i++) {
-      shipCoordinates.push([array[0], array[1] + i]);
+      shipCoordinates.push([array[0] + i, array[1]]);
     }
   }
   return shipCoordinates;
--- src/modules/test/ship.test.js
+++ src/modules/test/ship.test.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef */
 const shipCoordinates = require("../ship");
 
-test("shipCoordinates([0, 0], 5, 'Y') should return [[0,0], [0, 1], [0, 2], [0, 3], [0, 4]] coordinates", () => {
-  expect(shipCoordinates([0, 0], 5, "Y")).toEqual([
+test("shipCoordinates([0, 0], 5, 'X') should return [[0,0], [0, 1], [0, 2], [0, 3], [0, 4]] coordinates", () => {
+  expect(shipCoordinates([0, 0], 5, "X")).toEqual([
     [0, 0],
     [0, 1],
     [0, 2],
     [0, 3],
     [0, 4],
   ]);
-  expect(shipCoordinates([0, 0], 5, "X")).toEqual([
+  expect(shipCoordinates([0, 0], 5, "Y")).toEqual([
     [0, 0],
     [1, 0],
     [2, 0],
