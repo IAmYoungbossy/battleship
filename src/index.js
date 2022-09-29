e79761523107180e5dc2245be01e88364e45fc35
Letam Bossman Barinua <letambarinua@gmail.com>
2022-09-29 13:09:17 +0100

    Add dom-gameboard.js module for generating gameboard grid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import "./modules/style/style.css";
+
+const createGameboard = require("./modules/dom-interactions/dom-gameboard");
 const Ship = require("./modules/ship");
+
+createGameboard();
 
 console.log(Ship([1, 2], 5, "Y", [1, 2]));
--- src/modules/dom-interactions/dom-gameboard.js
+++ src/modules/dom-interactions/dom-gameboard.js
@@ -1,1 +1,21 @@
-const { body } = document;
+const createDomElement = require("./helper-function");
+
+function createGameboard() {
+  const { body } = document;
+  const boardContainer = createDomElement("div", { class: "board-container" });
+
+  function grids() {
+    const gameboard = createDomElement("div", { class: "gameboard" });
+    for (let i = 0; i < 100; i += 1) {
+      const grid = createDomElement("div", { class: "grid" });
+      gameboard.appendChild(grid);
+    }
+    return { gameboard };
+  }
+
+  boardContainer.appendChild(grids().gameboard);
+  boardContainer.appendChild(grids().gameboard);
+  body.appendChild(boardContainer);
+}
+
+module.exports = createGameboard;
