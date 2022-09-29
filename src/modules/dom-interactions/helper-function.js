0ac79ecf503a414b2b4c4da70424f92fd52a8ff9
Letam Bossman Barinua <letambarinua@gmail.com>
2022-09-29 11:43:54 +0100

    Add helper function module for creating DOM elements

---
+++ src/modules/dom-interactions/helper-function.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-use-before-define */
+// Helper function for creating DOM Elements
+
+function createDomElement(type, attributes) {
+  const newElement = document.createElement(`${type}`);
+  if (attributes == null) return newElement;
+  setAttributes(newElement, attributes);
+  return newElement;
+}
+
+function setAttributes(element, attributes) {
+  Object.entries(attributes).forEach(([key, value]) => {
+    element.setAttribute(key, value);
+  });
+}
+
+module.exports = createDomElement;
