2728f5c820cb0f6e5be0c8c46ba58bf34dc58f24
Letam Bossman Barinua <letambarinua@gmail.com>
2022-10-17 19:47:45 +0100

    Add hoverEffect feature for both single and 2 players

--- src/modules/dom-interactions/choose-player.js
+++ src/modules/dom-interactions/choose-player.js
@@ -29,10 +29,14 @@ function choosePlayer(playerName) {
   alignShipDiv.append(instruction2, axisBtn, continueBtn);
   instruction.textContent = `${playerName}, Choose Your Enemy.`;
   chooseDiv.append(instruction, btnDiv);
-  axisBtn.addEventListener("click", () => {
-    axisBtn.textContent =
-      axisBtn.textContent === "X AXIS" ? "Y AXIS" : "X AXIS";
+  continueBtn.addEventListener("click", () => {
+    lengthOfShip = 5;
+    counter = 5;
   });
+  const changeAxis = () =>
+    (axisBtn.textContent =
+      axisBtn.textContent === "X AXIS" ? "Y AXIS" : "X AXIS");
+  axisBtn.addEventListener("click", changeAxis);
   playAi.addEventListener(
     "click",
     insertBoard.bind(null, alignShipDiv, chooseDiv, playerShots)
@@ -51,11 +55,12 @@ function insertBoard(alignShipDiv, chooseDiv, playerShot) {
   head.classList.add("header");
   document.body.insertBefore(alignShipDiv, chooseDiv);
   document.body.replaceChild(boardContainer, chooseDiv);
-  addHover();
+  addHover(".player1-grid");
+  addHover(".player2-grid");
   playerShot();
 }
-function addHover() {
-  const playerBoard = document.querySelectorAll(".player1-grid");
+function addHover(className) {
+  const playerBoard = document.querySelectorAll(className);
   playerBoard.forEach(placeShipHover);
   function placeShipHover(grid, index) {
     const axis = `${index}`.split("");
@@ -83,14 +88,14 @@ function addHover() {
       const axisBtn = document.querySelector(".axis-btn");
       grid.classList.add("red");
       let index1, index2, index3, index4, index5;
-      if (axisBtn.textContent === "X AXIS") {
+      if (axisBtn && axisBtn.textContent === "X AXIS") {
         index1 = `${axis[0]}${axis[1]}`;
         index2 = `${axis[0]}${+axis[1] + 1}`;
         index3 = `${axis[0]}${+axis[1] + 2}`;
         index4 = `${axis[0]}${+axis[1] + 3}`;
         index5 = `${axis[0]}${+axis[1] + 4}`;
       }
-      if (axisBtn.textContent === "Y AXIS") {
+      if (axisBtn && axisBtn.textContent === "Y AXIS") {
         index1 = `${axis[0]}${axis[1]}`;
         index2 = `${+`${axis[0]}${axis[1]}` + 10}`;
         index3 = `${+`${axis[0]}${axis[1]}` + 20}`;
@@ -98,26 +103,26 @@ function addHover() {
         index5 = `${+`${axis[0]}${axis[1]}` + 40}`;
       }
       if (lengthOfShip === 5) {
-        if (index1.length === 2) indexArr.push(index1);
-        if (index2.length === 2) indexArr.push(index2);
-        if (index3.length === 2) indexArr.push(index3);
-        if (index4.length === 2) indexArr.push(index4);
-        if (index5.length === 2) indexArr.push(index5);
+        if (index1 && index1.length === 2) indexArr.push(index1);
+        if (index2 && index2.length === 2) indexArr.push(index2);
+        if (index3 && index3.length === 2) indexArr.push(index3);
+        if (index4 && index4.length === 2) indexArr.push(index4);
+        if (index5 && index5.length === 2) indexArr.push(index5);
       }
-      if (lengthOfShip === 4) {
-        if (index1.length === 2) indexArr.push(index1);
-        if (index2.length === 2) indexArr.push(index2);
-        if (index3.length === 2) indexArr.push(index3);
-        if (index4.length === 2) indexArr.push(index4);
+      if (index1 && lengthOfShip === 4) {
+        if (index1 && index1.length === 2) indexArr.push(index1);
+        if (index2 && index2.length === 2) indexArr.push(index2);
+        if (index3 && index3.length === 2) indexArr.push(index3);
+        if (index4 && index4.length === 2) indexArr.push(index4);
       }
       if (lengthOfShip === 3) {
-        if (index1.length === 2) indexArr.push(index1);
-        if (index2.length === 2) indexArr.push(index2);
-        if (index3.length === 2) indexArr.push(index3);
+        if (index1 && index1.length === 2) indexArr.push(index1);
+        if (index2 && index2.length === 2) indexArr.push(index2);
+        if (index3 && index3.length === 2) indexArr.push(index3);
       }
       if (lengthOfShip === 2) {
-        if (index1.length === 2) indexArr.push(index1);
-        if (index2.length === 2) indexArr.push(index2);
+        if (index1 && index1.length === 2) indexArr.push(index1);
+        if (index2 && index2.length === 2) indexArr.push(index2);
       }
       const addHoverEffect = (index) =>
         playerBoard[+index].classList.add("grey");
