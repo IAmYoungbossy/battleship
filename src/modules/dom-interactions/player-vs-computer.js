1136908c1af19a85a1cefce3c315e846cc74cd1a
Letam Bossman Barinua <letambarinua@gmail.com>
2022-10-20 18:17:15 +0100

    Disabled find-empty-spot.js

--- src/modules/dom-interactions/player-vs-computer.js
+++ src/modules/dom-interactions/player-vs-computer.js
@@ -1,4 +1,4 @@
-import { computerShots } from "./find-empty-spot";
+// import { computerShots } from "./find-empty-spot";
 import { announceWinner } from "./page-get-name";
 import { showShipsOnBoard } from "./place-ship";
 import { showShipsRandomly } from "./place-ship-random";
@@ -7,6 +7,10 @@ let hit;
 let time = 0;
 let stopHere = 0;
 let isSunkShipArray;
+let index2;
+let visitedIndex = null;
+let possibleValidShots = [];
+let firstHit;
 
 /** Checks for valid shots on ships */
 function shots(showShips, className) {
@@ -124,6 +128,90 @@ function isSunkShip(shipSunk, shipCoord, playerBoard) {
   if (shipSunk === true) shipCoord.forEach(addBlueBg);
 }
 
+function computerShots() {
+  const arrIndex = [];
+  const playerBoard2 = document.querySelectorAll(".player1-grid");
+  const push = (square, index) => {
+    if (square.classList[1] === "space") arrIndex.push(index);
+  };
+  playerBoard2.forEach(push);
+  const ranNum = Math.floor(Math.random() * (arrIndex.length - 1));
+  index2 = arrIndex[ranNum];
+  findEmptySpace(arrIndex, ranNum, playerBoard2);
+  const grid2 = playerBoard2[index2];
+  if (grid2) grid2.classList.remove("space");
+  return { grid2, index2, playerBoard2 };
+}
+
+function findEmptySpace(arrIndex, ranNum, playerBoard2) {
+  if (hit && visitedIndex >= 1) {
+    visitedIndex = visitedIndex - 1;
+    removeFromArray(0, +possibleValidShots[0] - 1);
+    removeFromArray(1, +possibleValidShots[1] + 1);
+    removeFromArray(2, +possibleValidShots[2] + 10);
+    removeFromArray(3, +possibleValidShots[3] - 10);
+    increaseVisitedIndex(playerBoard2);
+    visitedIndex++;
+  }
+  if (hit && visitedIndex === null) {
+    firstHit = +hit[0];
+    const splitHit = hit[0].split("");
+    possibleValidShots = [
+      splitHit[0] + (+splitHit[1] - 1),
+      splitHit[0] + (+splitHit[1] + 1),
+      +splitHit[0] + 1 + splitHit[1],
+      +splitHit[0] - 1 + splitHit[1],
+    ];
+    visitedIndex = 0;
+    index2 = possibleValidShots[visitedIndex];
+    increaseVisitedIndex(playerBoard2);
+    increaseVisitedIndex(playerBoard2);
+    increaseVisitedIndex(playerBoard2);
+    visitedIndex++;
+  }
+  if (hit === undefined && visitedIndex >= 1 && visitedIndex < 4) {
+    index2 = possibleValidShots[visitedIndex];
+    increaseVisitedIndex(playerBoard2);
+    increaseVisitedIndex(playerBoard2);
+    visitedIndex++;
+  }
+  if (isSunkShipArray && isSunkShipArray.includes(true)) {
+    possibleValidShots.splice(0);
+    index2 = arrIndex[ranNum];
+    visitedIndex = null;
+    firstHit = null;
+  }
+}
+
+function removeFromArray(index, expression) {
+  if (visitedIndex === index) {
+    index2 = expression;
+    possibleValidShots.splice(visitedIndex, 1, index2);
+  }
+}
+
+function increaseVisitedIndex(playerBoard2) {
+  if (
+    +index2 < 0 ||
+    +index2 > 99 ||
+    isNaN(+index2) ||
+    index2 === "010" ||
+    Array.from(playerBoard2[+index2].classList).includes("shots")
+  ) {
+    visitedIndex++;
+  }
+  index2 = +possibleValidShots[visitedIndex];
+  if (
+    firstHit &&
+    playerBoard2[+index2] &&
+    Array.from(playerBoard2[+index2].classList).includes("ship") &&
+    playerBoard2[+index2].classList[3] !== playerBoard2[firstHit].classList[2]
+  ) {
+    visitedIndex++;
+  }
+  index2 = +possibleValidShots[visitedIndex];
+}
+
 function resetPlayerVsComputer() {
   time = 0;
   stopHere = 0;
@@ -134,7 +222,5 @@ export {
   playerShots,
   validShots,
   stopHere,
-  resetPlayerVsComputer,
-  hit,
-  isSunkShipArray,
+  resetPlayerVsComputer
 };
