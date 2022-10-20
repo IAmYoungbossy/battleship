68e408886639b5bbf56d42d0dee81a867c8b90e5
Letam Bossman Barinua <letambarinua@gmail.com>
2022-10-20 11:38:12 +0100

    add find-empty-spot.js module to seperate concerns

--- src/modules/dom-interactions/player-vs-computer.js
+++ src/modules/dom-interactions/player-vs-computer.js
@@ -1,15 +1,12 @@
+import { computerShots } from "./find-empty-spot";
 import { announceWinner } from "./page-get-name";
 import { showShipsOnBoard } from "./place-ship";
 import { showShipsRandomly } from "./place-ship-random";
 
-let hit,
-  index2,
-  time = 0,
-  isSunkShipArray,
-  visitedIndex = null,
-  possibleValidShots = [],
-  stopHere = 0,
-  firstHit;
+let hit;
+let time = 0;
+let stopHere = 0;
+let isSunkShipArray;
 
 /** Checks for valid shots on ships */
 function shots(showShips, className) {
@@ -57,13 +54,7 @@ function alternateShots(
   const { grid2, index2, playerBoard2 } = computerShots();
   instruction.textContent = "Waiting for Computer's Shot.";
   validShots(
-    grid,
-    index,
-    receiveAttack,
-    playerBoard,
-    allShipsSunk,
-    instruction,
-    playerName
+    grid, index, receiveAttack, playerBoard, allShipsSunk, instruction, playerName
   );
 
   setTimeout(() => {
@@ -73,13 +64,7 @@ function alternateShots(
     if (stopHere === 1) return;
     instruction.textContent = `Waiting for ${playerName}'s Shot.`;
     const { hitCoord } = validShots(
-      grid2,
-      index2,
-      receiveAttack2,
-      playerBoard2,
-      allShipsSunk2,
-      instruction,
-      AIName
+      grid2, index2, receiveAttack2, playerBoard2, allShipsSunk2, instruction, AIName
     );
     hit = hitCoord;
   }, 1500);
@@ -87,13 +72,7 @@ function alternateShots(
 
 /** Colours valid shots red */
 function validShots(
-  grid,
-  index,
-  receiveAttack,
-  playerBoard,
-  allShipsSunk,
-  instruction,
-  playerName
+  grid, index, receiveAttack, playerBoard, allShipsSunk, instruction, playerName
 ) {
   const axis = `${index}`.split("");
   if (axis.length === 1) axis.unshift("0");
@@ -144,87 +123,6 @@ function isSunkShip(shipSunk, shipCoord, playerBoard) {
     });
   if (shipSunk === true) shipCoord.forEach(addBlueBg);
 }
-function computerShots() {
-  const arrIndex = [];
-  const playerBoard2 = document.querySelectorAll(".player1-grid");
-  const push = (square, index) => {
-    if (square.classList[1] === "space") arrIndex.push(index);
-  };
-  playerBoard2.forEach(push);
-  const ranNum = Math.floor(Math.random() * (arrIndex.length - 1));
-  index2 = arrIndex[ranNum];
-  findValidShots(arrIndex, ranNum, playerBoard2);
-  const grid2 = playerBoard2[index2];
-  if (grid2) grid2.classList.remove("space");
-  return { grid2, index2, playerBoard2 };
-}
-
-function findValidShots(arrIndex, ranNum, playerBoard2) {
-  if (hit && visitedIndex >= 1) {
-    visitedIndex = visitedIndex - 1;
-    removeFromArray(0, +possibleValidShots[0] - 1);
-    removeFromArray(1, +possibleValidShots[1] + 1);
-    removeFromArray(2, +possibleValidShots[2] + 10);
-    removeFromArray(3, +possibleValidShots[3] - 10);
-    increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
-  }
-  if (hit && visitedIndex === null) {
-    firstHit = +hit[0];
-    const splitHit = hit[0].split("");
-    possibleValidShots = [
-      splitHit[0] + (+splitHit[1] - 1),
-      splitHit[0] + (+splitHit[1] + 1),
-      +splitHit[0] + 1 + splitHit[1],
-      +splitHit[0] - 1 + splitHit[1],
-    ];
-    visitedIndex = 0;
-    index2 = possibleValidShots[visitedIndex];
-    increaseVisitedIndex(playerBoard2);
-    increaseVisitedIndex(playerBoard2);
-    increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
-  }
-  if (hit === undefined && visitedIndex >= 1 && visitedIndex < 4) {
-    index2 = possibleValidShots[visitedIndex];
-    increaseVisitedIndex(playerBoard2);
-    increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
-  }
-  if (isSunkShipArray && isSunkShipArray.includes(true)) {
-    possibleValidShots.splice(0);
-    index2 = arrIndex[ranNum];
-    visitedIndex = null;
-    firstHit = null;
-  }
-}
-function removeFromArray(index, expression) {
-  if (visitedIndex === index) {
-    index2 = expression;
-    possibleValidShots.splice(visitedIndex, 1, index2);
-  }
-}
-function increaseVisitedIndex(playerBoard2) {
-  if (
-    +index2 < 0 ||
-    +index2 > 99 ||
-    isNaN(+index2) ||
-    index2 === "010" ||
-    Array.from(playerBoard2[+index2].classList).includes("shots")
-  ) {
-    visitedIndex++;
-  }
-  index2 = +possibleValidShots[visitedIndex];
-  if (
-    firstHit &&
-    playerBoard2[+index2] &&
-    Array.from(playerBoard2[+index2].classList).includes("ship") &&
-    playerBoard2[+index2].classList[3] !== playerBoard2[firstHit].classList[2]
-  ) {
-    visitedIndex++;
-  }
-  index2 = +possibleValidShots[visitedIndex];
-}
 
 function resetPlayerVsComputer() {
   time = 0;
@@ -232,4 +130,11 @@ function resetPlayerVsComputer() {
 }
 
 const playerShots = shots.bind(null, showShipsRandomly, "player2-grid");
-export { playerShots, validShots, stopHere, resetPlayerVsComputer };
+export {
+  playerShots,
+  validShots,
+  stopHere,
+  resetPlayerVsComputer,
+  hit,
+  isSunkShipArray,
+};
