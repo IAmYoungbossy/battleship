d544d06fe442df9dd417e7fa9a5b7b9cf59ae543
Letam Bossman Barinua <letambarinua@gmail.com>
2022-10-22 22:26:25 +0100

    Refactor function logic in find-empty-spot.js modules

--- src/modules/dom-interactions/find-empty-spot.js
+++ src/modules/dom-interactions/find-empty-spot.js
@@ -3,24 +3,25 @@ import { hit, isSunkShipArr } from "./player-vs-computer";
 let index2;
 let visitedIndex = null;
 let possibleValidShots = [];
+let currentShipClass;
 let firstHit;
 
 function computerShots() {
   const arrIndex = [];
   const playerBoard2 = document.querySelectorAll(".player1-grid");
-  const push = (square, index) => {
+  const pushIt = (square, index) => {
     if (square.classList[1] === "space") arrIndex.push(index);
   };
-  playerBoard2.forEach(push);
+  playerBoard2.forEach(pushIt);
   const ranNum = Math.floor(Math.random() * (arrIndex.length - 1));
   index2 = arrIndex[ranNum];
-  findEmptySpace(arrIndex, ranNum, playerBoard2);
-  const grid2 = playerBoard2[index2];
+  findValidHitOnShip(arrIndex, ranNum, playerBoard2);
+  const grid2 = playerBoard2[+index2];
   if (grid2) grid2.classList.remove("space");
   return { grid2, index2, playerBoard2 };
 }
 
-function findEmptySpace(arrIndex, ranNum, playerBoard2) {
+function findValidHitOnShip(arrIndex, ranNum, playerBoard2) {
   if (hit && visitedIndex >= 1) {
     visitedIndex = visitedIndex - 1;
     removeFromArray(0, +possibleValidShots[0] - 1);
@@ -28,7 +29,7 @@ function findEmptySpace(arrIndex, ranNum, playerBoard2) {
     removeFromArray(2, +possibleValidShots[2] + 10);
     removeFromArray(3, +possibleValidShots[3] - 10);
     increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
+    visitedIndex += 1;
   }
   if (hit && visitedIndex === null) {
     firstHit = +hit[0];
@@ -40,17 +41,15 @@ function findEmptySpace(arrIndex, ranNum, playerBoard2) {
       +splitHit[0] - 1 + splitHit[1],
     ];
     visitedIndex = 0;
-    index2 = possibleValidShots[visitedIndex];
     increaseVisitedIndex(playerBoard2);
     increaseVisitedIndex(playerBoard2);
     increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
+    visitedIndex += 1;
   }
   if (hit === undefined && visitedIndex >= 1 && visitedIndex < 4) {
-    index2 = possibleValidShots[visitedIndex];
     increaseVisitedIndex(playerBoard2);
     increaseVisitedIndex(playerBoard2);
-    visitedIndex++;
+    visitedIndex += 1;
   }
   if (isSunkShipArr && isSunkShipArr.includes(true)) {
     possibleValidShots = [];
@@ -68,25 +67,43 @@ function removeFromArray(index, expression) {
 }
 
 function increaseVisitedIndex(playerBoard2) {
-  if (
-    +index2 < 0 ||
-    +index2 > 99 ||
-    isNaN(+index2) ||
-    index2 === "010" ||
+  index2 = possibleValidShots[visitedIndex];
+  if (+index2 < 0) {
+    visitedIndex += 1;
+    index2 = possibleValidShots[visitedIndex];
+  } else if (+index2 > 99) {
+    visitedIndex += 1;
+    index2 = possibleValidShots[visitedIndex];
+  } else if (Number.isNaN(+index2)) {
+    visitedIndex += 1;
+    index2 = possibleValidShots[visitedIndex];
+  } else if (index2 === "010") {
+    visitedIndex += 1;
+    index2 = possibleValidShots[visitedIndex];
+  } else if (
+    playerBoard2[+index2] &&
     Array.from(playerBoard2[+index2].classList).includes("shots")
   ) {
-    visitedIndex++;
+    visitedIndex += 1;
+    index2 = possibleValidShots[visitedIndex];
   }
-  index2 = +possibleValidShots[visitedIndex];
   if (
     firstHit &&
     playerBoard2[+index2] &&
-    Array.from(playerBoard2[+index2].classList).includes("ship") &&
-    playerBoard2[+index2].classList[3] !== playerBoard2[firstHit].classList[2]
+    Array.from(playerBoard2[+index2].classList).includes("ship")
   ) {
-    visitedIndex++;
+    Array.from(playerBoard2[firstHit].classList).forEach((klass) => {
+      const splitToArray = klass.split("");
+      const lastLetter = splitToArray[klass.length - 1];
+      if (!Number.isNaN(+lastLetter)) currentShipClass = klass;
+    });
+    if (
+      !Array.from(playerBoard2[+index2].classList).includes(currentShipClass)
+    ) {
+      visitedIndex += 1;
+      index2 = possibleValidShots[visitedIndex];
+    }
   }
-  index2 = +possibleValidShots[visitedIndex];
 }
 
 export { computerShots };
